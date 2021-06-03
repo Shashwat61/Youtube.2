7d96527804e2849b6570cd7aa63cd293e6d08160
shashwat <shashwat1461.cse18@chitkara.edu.in>
2021-06-03 20:06:18 +0530

    setup tailwin

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="w-full h-screen flex justify-center items-center">
+       <h1 className="text-yellow-500">Hello tailwind</h1>
+     </div>
+    </div>
+  );
+}
+
+export default App;
