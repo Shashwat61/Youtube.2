0617b2138658ad5752872fab354ee24447d0c47c
shashwat <shashwat1461.cse18@chitkara.edu.in>
2021-06-19 18:22:57 +0530

    serviceworker added manifest added offline file added

---
+++ public/serviceWorker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME='cache-v1'
+const preResources=['/','index.html','offline.html']
+
+const self=this
+
+//install serviceworker
+self.addEventListener('install',(e)=>{
+    console.log('service worker instal event!')
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache)=>{
+            console.log('opened cache')
+            return cache.addAll(preResources)
+        })
+    )
+})
+
+//activate serviceworker
+self.addEventListener('activate',(e)=>{
+    console.log('service worker activated event')
+const cachewhiteList=[]
+cachewhiteList.push(CACHE_NAME)
+e.waitUntil(
+    caches.keys()
+    .then((cacheNames)=>Promise.all(
+        cacheNames.map((cacheName)=>{
+            if(!cachewhiteList.includes(cacheName))
+            return caches.delete(cacheName)
+        })
+    ))
+)
+})
+
+//listen for request
+self.addEventListener('fetch',(e)=>{
+    console.log('fetch intercepted for:',e.request.url)
+    e.respondWith(
+        caches.match(e.request)
+        .then(()=>{
+            return fetch(e.request)
+            .catch(()=>caches.match('offline.html'))
+        })
+    )
+})
--- src/components/screens/notfound/NotFound.js
+++ src/components/screens/notfound/NotFound.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const NotFound = () => {
     return (
         <div className="flex space-y-3 text-textcolor h-screen flex-col items-center justify-center">
-            <img className="" src="https://www.gstatic.com/youtube/src/web/htdocs/img/monkey.png" alt=""/>
+            <img className="" src="https://www.gstatic.com/youtube/src/web/htdocs/img/monkey.png" alt="notfound"/>
             <span>Oops! Page you are looking for does not exist</span>
             <Link className="flex items-center space-x-3" to="/">
-            <img className="h-12 w-16" src="http://pngimg.com/uploads/youtube/youtube_PNG2.png" alt=""/>
+            <img className="h-12 w-16" src="http://pngimg.com/uploads/youtube/youtube_PNG2.png" alt="gobackicon"/>
             <span to="/">Go Back</span>
             </Link>
         </div>
